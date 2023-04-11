5d383a990b489299a9878f7b160d89db12bb947b
mastero <castro.alejandro17@gmail.com>
2023-04-11 08:53:19 -0600

    Init Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    mysql.query('SELECT * FROM prueba', (error, results, fields) => {
+        if (error) {
+            console.error('Error al obtener datos de la base de datos:', error);
+            res.status(500).send('Error al obtener datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    mysql.query('SELECT * FROM prueba WHERE id = ?', id, (error, results, fields) => {
+      if (error) {
+        console.error('Error al obtener datos de la base de datos:', error);
+        res.status(500).send('Error al obtener datos de la base de datos.');
+        return;
+      }
+      if (results.length === 0) {
+        res.status(404).send('No se encontró ningún usuario con el ID proporcionado.');
+        return;
+      }
+      res.send(results[0]);
+    });
+  });
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    mysql.query('INSERT INTO prueba SET ?', data, (error, results, fields) => {
+        if (error) {
+            console.error('Error al insertar datos en la base de datos:', error);
+            res.status(500).send('Error al insertar datos en la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    mysql.query('UPDATE prueba SET ? WHERE id = ?', [data, id], (error, results, fields) => {
+        if (error) {
+            console.error('Error al actualizar datos en la base de datos:', error);
+            res.status(500).send('Error al actualizar datos en la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    mysql.query('DELETE FROM prueba WHERE id = ?', id, (error, results, fields) => {
+        if (error) {
+            console.error('Error al eliminar datos de la base de datos:', error);
+            res.status(500).send('Error al eliminar datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000.');
+});
