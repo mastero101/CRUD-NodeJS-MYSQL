2c5e5fc04eeb0dc992092e8880e2e53603e09bc5
mastero101 <mastershift_101@gotmail.com>
2023-04-23 19:55:43 +0000

    add app3.js

---
+++ app3.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
+const mysql = require('./db2');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+const options = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem')
+};
+
+app.get('/', (req, res) => {
+    mysql.query('SELECT * FROM componentes', (error, results, fields) => {
+        if (error) {
+            console.error('Error al obtener datos de la base de datos:', error);
+            res.status(500).send('Error al obtener datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    mysql.query('SELECT * FROM componentes WHERE id = ?', id, (error, results, fields) => {
+      if (error) {
+        console.error('Error al obtener datos de la base de datos:', error);
+        res.status(500).send('Error al obtener datos de la base de datos.');
+        return;
+      }
+      if (results.length === 0) {
+        res.status(404).send('No se encontró ningún usuario con el ID proporcionado.');
+        return;
+      }
+      res.send(results[0]);
+    });
+});
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    mysql.query('INSERT INTO componentes SET ?', data, (error, results, fields) => {
+        if (error) {
+            console.error('Error al insertar datos en la base de datos:', error);
+            res.status(500).send('Error al insertar datos en la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    mysql.query('UPDATE componentes SET ? WHERE id = ?', [data, id], (error, results, fields) => {
+        if (error) {
+            console.error('Error al actualizar datos en la base de datos:', error);
+            res.status(500).send('Error al actualizar datos en la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    mysql.query('DELETE FROM componentes WHERE id = ?', id, (error, results, fields) => {
+        if (error) {
+            console.error('Error al eliminar datos de la base de datos:', error);
+            res.status(500).send('Error al eliminar datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+https.createServer(options, app).listen(443, () => {
+    console.log('Servidor escuchando en el puerto 443.');
+});
